5814b4febe14d850d5b058ad77cf98440042ac37
a181545 <114413070+a181545@users.noreply.github.com>
2022-11-06 21:00:06 -0600

    Add files via upload

---
+++ PantallasProyectoParte1/PantallasProyectoParte1/EditorEquipos.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace PantallasProyectoParte1
+{
+    public class EditorEquipos
+    {
+        private ConexionBaseDatos Conexion = new ConexionBaseDatos();
+        private SqlCommand Comando = new SqlCommand();
+        private SqlDataReader LeerFilas;
+        private SqlDataReader LeerID;
+
+        public void InsertarEspecificacionesPrincipales(Especificaciones_Principales insercion1)
+        {
+            Comando.Connection = Conexion.AbrirConexion();
+            Comando.CommandText = "InsertarEspecificacionesPrincipales";
+            Comando.CommandType = CommandType.StoredProcedure;
+            Comando.Parameters.AddWithValue("@procesador", insercion1.Procesador);
+            Comando.Parameters.AddWithValue("@placa_madre", insercion1.PlacaMadre);
+            Comando.Parameters.AddWithValue("@graficos", insercion1.Graficos);
+            Comando.Parameters.AddWithValue("@memoria_ram", insercion1.MemoriaRAM);
+            Comando.Parameters.AddWithValue("@n_ventiladores", insercion1.NVentiladores);
+            Comando.Parameters.AddWithValue("@tipo_fuente_poder", insercion1.TipoFuentePoder);
+            Comando.Parameters.AddWithValue("@potencia_fuente_poder", insercion1.PotenciaFuentePoder);
+            Comando.Parameters.AddWithValue("@tipo_fuente_alimentacion", insercion1.TipoFuenteAlimentacion);
+            Comando.Parameters.AddWithValue("@potencia_fuente_alimentacion", insercion1.PotenciaFuenteAlimentacion);
+            Comando.ExecuteNonQuery();
+            Comando.Parameters.Clear();
+        }
+
+        public void InsertarAlmacenamiento(Almacenamiento insercion2)
+        {
+            Comando.Connection = Conexion.AbrirConexion();
+            Comando.CommandText = "InsertarAlmacenamiento";
+            Comando.CommandType = CommandType.StoredProcedure;
+            Comando.Parameters.AddWithValue("@tipo_almacenamiento", insercion2.TipoAlmacenamiento);
+            Comando.Parameters.AddWithValue("@alm_total", insercion2.AlmacenamientoTotal);
+            Comando.Parameters.AddWithValue("@alm_disponible", insercion2.AlmacenamientoDisponible);
+            Comando.Parameters.AddWithValue("@prog_instalados", insercion2.ProgramasInstalados);
+            Comando.ExecuteNonQuery();
+            Comando.Parameters.Clear();
+        }
+
+        public void InsertarCondicionesExternas(Condiciones_Externas insercion3)
+        {
+            Comando.Connection = Conexion.AbrirConexion();
+            Comando.CommandText = "InsertarCondicionesExternas";
+            Comando.CommandType = CommandType.StoredProcedure;
+            Comando.Parameters.AddWithValue("@cond_gabinete", insercion3.CondGabinete);
+            Comando.Parameters.AddWithValue("@cond_teclado", insercion3.CondTeclado);
+            Comando.Parameters.AddWithValue("@cond_mouse", insercion3.CondMouse);
+            Comando.Parameters.AddWithValue("@cond_monitor", insercion3.CondMonitor);
+            Comando.ExecuteNonQuery();
+            Comando.Parameters.Clear();
+        }
+
+        public void InsertarConclusiones(Conclusiones_Del_Equipo insercion4)
+        {
+            Comando.Connection = Conexion.AbrirConexion();
+            Comando.CommandText = "InsertarConclusiones";
+            Comando.CommandType = CommandType.StoredProcedure;
+            Comando.Parameters.AddWithValue("@ultimo_mantenimiento", insercion4.UltimoMantenimiento);
+            Comando.Parameters.AddWithValue("@cond_generales", insercion4.CondicionesGenerales);
+            Comando.Parameters.AddWithValue("@observaciones", insercion4.Observaciones);
+            Comando.ExecuteNonQuery();
+            Comando.Parameters.Clear();
+            //Seleccionar el maximo id de la tablas
+            Comando.CommandText = "MaximoIDPrueba";
+            Comando.CommandType = CommandType.StoredProcedure;
+            LeerID=Comando.ExecuteReader();
+            
+
+        }
+
+        public void GuardarEquipo(NombreEquipos guardado)
+        {
+            Comando.Connection = Conexion.AbrirConexion();
+            Comando.CommandText = "InsertarEquiposIDsPrueba";
+            Comando.CommandType = CommandType.StoredProcedure;
+            Comando.Parameters.AddWithValue("@equipo", guardado.Equipos);
+            Comando.Parameters.AddWithValue("@idEspecifPrinF", guardado.idEspecifPrinF);
+            Comando.Parameters.AddWithValue("@idCondExtF", guardado.idCondExtF);
+            Comando.Parameters.AddWithValue("@idAlmacenamiento", guardado.idAlmacenamientoF);
+            Comando.Parameters.AddWithValue("@idConclusiones", guardado.idConclusionesF);
+            Comando.ExecuteNonQuery();
+            Comando.Parameters.Clear();
+        }
+
+        public DataTable VistaEquipos()
+        {
+            DataTable dt = new DataTable();
+            Comando.Connection = Conexion.AbrirConexion();
+            Comando.CommandText = "MostrarEquiposPrueba";
+            Comando.CommandType = CommandType.StoredProcedure;
+            LeerFilas=Comando.ExecuteReader();
+            dt.Load(LeerFilas);
+            LeerFilas.Close();
+            Conexion.CerrarConexion();
+            return dt;
+        }
+
+    }
+}
---
+++ PantallasProyectoParte1/PantallasProyectoParte1/Pantalla_Agregar_Equipo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PantallasProyectoParte1
+{
+    public partial class Pantalla_Agregar_Equipo : Form
+    {
+        EditorEquipos ee = new EditorEquipos();
+        public Pantalla_Agregar_Equipo()
+        {
+            InitializeComponent();
+        }
+
+        private void Pantalla_Agregar_Equipo_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuardarCambiosNuevo_Click(object sender, EventArgs e)
+        {
+            Especificaciones_Principales EP = new Especificaciones_Principales(txtProcNuevo.Text, txtPMNuevo.Text, txtGraficosNuevo.Text, 
+                int.Parse(txtRAMNuevo.Text), int.Parse(txtVentNuevo.Text), cboFuentePoderNuevo.Text, int.Parse(txtPotFuenteNuevo.Text), 
+                cboFuenteAlimNuevo.Text, int.Parse(txtPotAlimNuevo.Text));
+            ee.InsertarEspecificacionesPrincipales(EP);
+
+            Almacenamiento alm = new Almacenamiento(cboTipoAlmNuevo.Text, int.Parse(txtAlmTotNuevo.Text),
+                int.Parse(txtAlmDispNuevo.Text), int.Parse(txtProgInstNuevo.Text));
+            ee.InsertarAlmacenamiento(alm);
+
+            Condiciones_Externas ce = new Condiciones_Externas(cboGabineteNuevo.Text, cboTecladoNuevo.Text, cboMouseNuevo.Text, cboMonitorNuevo.Text);
+            ee.InsertarCondicionesExternas(ce);
+
+            Conclusiones_Del_Equipo cde = new Conclusiones_Del_Equipo(dtpUltMantNuevo.Value, cboCondGenNuevo.Text, txtObservacionesNuevo.Text);
+            ee.InsertarConclusiones(cde);
+
+            MessageBox.Show("Los datos se han introducido correctamente");
+
+            Close();
+        }
+    }
+}
